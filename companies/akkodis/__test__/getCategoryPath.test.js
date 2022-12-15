cb44649bf934baf4f6d6fe1e7ce51f67a04ce86f
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2022-12-15 13:38:54 +0100

    test(getCategoryPath): update the description tests to simplify them

--- companies/akkodis/__test__/getCategoryPath.test.js
+++ companies/akkodis/__test__/getCategoryPath.test.js
@@ -2,15 +2,15 @@ const { getCategoryPath } = require("../functions.js");
 const { categories } = require("../data.js");
 
 describe("Get full category path including parent and subcategories", () => {
-	test("if 'category4' is selected his full path is '/category1/category3/category4'", () => {
+	test("'category4' is '/category1/category3/category4'", () => {
 		expect(getCategoryPath(categories, "category4")).toBe("/category1/category3/category4");
 	});
 
-	test("if 'category2' is selected his full path is '/category1/category2'", () => {
+	test("'category2' is '/category1/category2'", () => {
 		expect(getCategoryPath(categories, "category2")).toBe("/category1/category2");
 	});
 
-	test("if 'category5' is selected his full path is '/category5'", () => {
+	test("'category5' is '/category5'", () => {
 		expect(getCategoryPath(categories, "category5")).toBe("/category5");
 	});
 });
