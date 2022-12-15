07f7bc60c2071531ba0f840ed093bcb79e5c5f03
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2022-12-15 11:24:17 +0100

    refactor(getCategoryNumber): remove code not used

--- companies/akkodis/functions.js
+++ companies/akkodis/functions.js
@@ -1,16 +1,7 @@
 // TO-DO: Implement this function
 
-const getCategoryNumber = categoryName => {
-	const number = categoryName.replace("category", "");
-
-	return number;
-};
-
 const getCategoryPath = (categories, categoryName) => {
 	let path = "";
-
-	const categoryNumber = getCategoryNumber(categoryName);
-	console.log(categoryNumber);
 
 	categories.forEach(category => {
 		if (category.name != categoryName) {
