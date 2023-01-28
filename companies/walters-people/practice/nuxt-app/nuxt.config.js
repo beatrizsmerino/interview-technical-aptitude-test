5e68b84d638fcfceddd1ab3b6625ada08fb15573
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-01-28 04:02:28 +0100

    fix(css): css styles only work on `index` page.
    Error:
    - Found `link` html tag inside the component.
    Styles from `Tailwindcss` library cannot be used outside the `Tutorial` component.
    
    Solution:
    - Load link to `Tailwindcss` library as link in nuxt config instead of html tag inside component.

--- companies/walters-people/practice/nuxt-app/nuxt.config.js
+++ companies/walters-people/practice/nuxt-app/nuxt.config.js
@@ -14,7 +14,13 @@ export default {
 			{ hid: 'description', name: 'description', content: '' },
 			{ name: 'format-detection', content: 'telephone=no' },
 		],
-		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{
+				href: 'https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css',
+				rel: 'stylesheet',
+			},
+		],
 	},
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
