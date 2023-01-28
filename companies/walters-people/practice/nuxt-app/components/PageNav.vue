865b7d64511f3fd8c4bbac5128b819a1f0bcf5d9
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-01-28 03:24:34 +0100

    feat(PageNav): create a navigation on `index` page.
    Steps:
    - Create the `PageNav` component.
    - Create `linkList` as `data` vuex to print with a loop, the list of links.
    - Use the `el-menu` and `el-menu-item` components of the `ElementUI` library to create a navigation menu.
    - Create `setLinkIDDefaultActive` as `method` vuex to save the ID of the selected link, inside `linkDefaultActive` data vuex, at the instant the component is mounted.
    - Set the link ID saved into `linkDefaultActive` to the `el-menu` component with the `default-active` prop.
    - In the `el-menu` component, use the `default-active` property to set the index (`linkDefaultActive`) of the active menu on page load. Also, use the `router` property as `true` to activate the `vue-router` mode, so the `index` will be used as `route` to activate the route action. Use with `default-active` to set the active element in the load.
    - In the `el-menu-item` component, use the `index` prop as the unique ID and the `route` prop as the `vue-router` object.
    - Use and import the `PageNav` component inside the `index` page.
    
    Refrence:
    - https://element-plus.org/en-US/component/menu.html#top-bar

---
+++ companies/walters-people/practice/nuxt-app/components/PageNav.vue
@@ -0,0 +1,63 @@
+<template>
+	<el-menu
+		:router="true"
+		:default-active="linkDefaultActive"
+		class="page-nav"
+		mode="horizontal"
+		background-color="#003543"
+		text-color="#fff"
+		active-text-color="#00DC82"
+	>
+		<el-menu-item
+			v-for="item in linkList"
+			:key="item.id"
+			:index="item.id"
+			:route="item"
+		>
+			{{ item.title }}
+		</el-menu-item>
+	</el-menu>
+</template>
+
+<script>
+export default {
+	name: 'PageNav',
+	data() {
+		return {
+			linkList: [
+				{
+					id: '1',
+					name: 'index',
+					title: 'Home',
+					path: '/',
+				},
+				{
+					id: '2',
+					name: 'user-list',
+					title: 'User list',
+					path: '/user-list',
+				},
+			],
+			linkDefaultActive: null,
+		}
+	},
+	mounted() {
+		this.setLinkIDDefaultActive()
+	},
+	methods: {
+		setLinkIDDefaultActive() {
+			const currentLink = this.linkList.filter(
+				item => item.name === this.$route.name,
+			)[0]
+			this.linkDefaultActive = currentLink.id
+		},
+	},
+}
+</script>
+
+<style lang="css" scoped>
+.page-nav {
+	display: flex;
+	justify-content: flex-end;
+}
+</style>
--- companies/walters-people/practice/nuxt-app/pages/index.vue
+++ companies/walters-people/practice/nuxt-app/pages/index.vue
@@ -1,16 +1,19 @@
 <template>
 	<div>
+		<PageNav />
 		<Tutorial />
 		<UserList />
 	</div>
 </template>
 
 <script>
+	import PageNav from '@/components/PageNav';
 	import Tutorial from '@/components/Tutorial';
 
 	import UserList from '@/components/UserList';
 	export default {
 		Tutorial,
-		UserList
+		UserList,
+		PageNav
 	};
 </script>
