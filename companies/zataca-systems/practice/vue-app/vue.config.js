6ae926fa6445c003fcb879d2ae910dda1830e20c
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-02-04 20:28:27 +0100

    build(deps,config): add packages, configuration and npm scripts to use `prettier`, `eslint`, `babel` and `stylelint`
    Steps:
    - To use `stylelint`:
    Add the npm packages: `stylelint`, `stylelint-config-prettier`, `stylelint-config-property-sort-order-smacss`, `stylelint-config-sass-guidelines`, `stylelint-config-standard`, `stylelint-scss` and `stylelint-webpack-plugin`.
    Create `stylelint.config.js` and `.stylelintignore` files.
    - To use `eslint`:
    Upgrade to the major versions npm packages: `eslint`, `eslint-plugin-prettier` and `eslint-plugin-vue`.
    Install the npm packages: `eslint-plugin-json` and `eslint-webpack-plugin`.
    Update `.eslint.js` file and create `.eslintignore` file.
    - To use `prettier`:
    Create `.prettier` and `.prettierignore` files.
    - To use `babel`:
    Uninstall the deprecated `babel-eslint` package, and use `@babel/eslint-parser` instead.
    Add the npm package `@babel/core`.
    - To use `editorconfig`:
    Create the `editorconfig` file
    - To use `stylelint` and `eslint`:
    To format the files and lint on save, use the `stylelint-webpack-plugin` and `eslint-webpack-plugin` packages, add the `vsc` configuration and add the configuration to the `vue.config.js` file.
    - Update npm scripts:
    Add `prettier:fix`, `eslint:fix` and `stylelint:fix`.
    Update the `lint` npm script by executing the three consecutive commands: `prettier:fix`, `eslint:fix` and `stylelint:fix`.

--- companies/zataca-systems/practice/vue-app/vue.config.js
+++ companies/zataca-systems/practice/vue-app/vue.config.js
@@ -1,4 +1,8 @@
+const ESLintPlugin = require('eslint-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
 module.exports = {
+
 	// Add configuration for use Dart sass/scss and compile files of 'assets' folder
 	css: {
 		loaderOptions: {
@@ -12,5 +16,30 @@ module.exports = {
 				additionalData: `@import "@/assets/scss/styles.scss";`,
 			},
 		},
+	},
+
+	// Emit 'eslint' errors and warnings in the console
+	// https://cli.vuejs.org/config/#lintonsave
+	lintOnSave: true,
+
+	configureWebpack: {
+		plugins: [
+
+			// Add configuration for autofix eslint errors
+			new ESLintPlugin({
+				fix: true,
+				files: [
+					'src/**/*.{vue,js}'
+				]
+			}),
+
+			// Add configuration for autofix stylelint errors
+			new StyleLintPlugin({
+				fix: true,
+				files: [
+					'src/**/*.{vue,scss}'
+				]
+			})
+		]
 	},
 };
