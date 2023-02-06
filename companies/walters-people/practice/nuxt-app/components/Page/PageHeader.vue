8a6577c078224a265f5d81846a634d3c30f7671a
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-02-06 06:57:08 +0100

    feat(LogoNuxt): add logo to header page
    Steps to follow:
    - Import and use the `LogoNuxt` component in the `PageHeader` component.
    - Add the `symbol` value to the `logotype` prop of the `LogoNuxt` component.
    - Use the `page-header__logo` to visually position the `LogoNuxt` to the left and centered vertically in the header.

--- companies/walters-people/practice/nuxt-app/components/Page/PageHeader.vue
+++ companies/walters-people/practice/nuxt-app/components/Page/PageHeader.vue
@@ -1,15 +1,36 @@
 <template>
-	<el-header>
+	<el-header class="page-header">
+		<LogoNuxt
+			class="page-header__logo"
+			logotype="symbol"
+		/>
 		<PageNav />
 	</el-header>
 </template>
 
 <script>
+	import LogoNuxt from "@/components/Logo/LogoNuxt";
 	import PageNav from "@/components/Page/PageNav";
+
 	export default {
 		"name": "PageHeader",
 		"components": {
+			LogoNuxt,
 			PageNav,
 		},
 	};
 </script>
+
+<style lang="scss" scoped>
+	.page-header {
+		position: relative;
+
+		&__logo {
+			position: absolute;
+			z-index: 999;
+			top: 50%;
+			left: 2rem;
+			transform: translateY(-50%);
+		}
+	}
+</style>
