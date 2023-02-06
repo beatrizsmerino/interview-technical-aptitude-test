3f4c07b581a3300a5322cdc80ef668234d500968
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-02-06 07:59:55 +0100

    refactor(PageNav): some components may be reusable
    Steps:
    - Create the `PageNav.vue` component.
    - Import and use the `PageNav` component inside the `PageHeader` component.
    - Rename the class `nav` to `page-nav`.
    - Move some styles of `page-nav` to `page-header`.

---
+++ companies/zataca-systems/practice/vue-app/src/components/Page/PageNav.vue
@@ -0,0 +1,85 @@
+<template>
+	<nav class="page-nav">
+		<ul class="page-nav__list">
+			<li
+				v-for="link in getLinkList"
+				:key="link.id"
+				class="page-nav__item"
+			>
+				<router-link
+					class="page-nav__link"
+					:to="link.path"
+				>
+					{{ link.name }}
+				</router-link>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script>
+	export default {
+		"name": "PageNav",
+		data() {
+			return {
+				"linkList": [
+					{
+						"name": "Home",
+						"path": "/",
+					},
+					{
+						"name": "About",
+						"path": "/about",
+					},
+					{
+						"name": "Products",
+						"path": "/products",
+					},
+				],
+			};
+		},
+		"computed": {
+			getLinkList() {
+				return this.linkList;
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.page-nav {
+		&__list {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			margin: 0;
+			padding: 0;
+			list-style: none;
+		}
+
+		&__item {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+
+			&:not(:last-child) {
+				&:after {
+					content: '|';
+					display: inline-block;
+					margin: 0 0.5rem;
+				}
+			}
+		}
+
+		&__link {
+			padding: 0.5rem;
+			color: $color-brand-2;
+			font-weight: bold;
+			text-decoration: none;
+
+			&.router-link-exact-active {
+				color: $color-brand-1;
+			}
+		}
+	}
+</style>
