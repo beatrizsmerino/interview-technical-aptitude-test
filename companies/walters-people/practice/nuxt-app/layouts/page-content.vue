ee98afad3a54cc9ead2cdf629c617c6a4fd50073
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-02-06 09:42:01 +0100

    feat(layout): create and use another layout for the pages with content
    Steps:
    - Create the `page-content.vue` file as layout.
    - Move the `.page-main` custom styles from the `user-list` page to the `page-content` layout. It was to be used only on the `user-list` page, and created for pages with margins on the sides, so that the background is not cut off, which will happen if they are used on the `index` page.
    - Import and use the `PageTitle` component inside the `page-content` layout.
    - Creates the page title, `pageTitle`, dynamically in the `page-content` layout.
    The title is obtained from the path name with the `getPageTitle` computed property. And it is formatted with the `formatPageTitle` method, removing the middle hyphens and capitalizing the first letter of the title.

---
+++ companies/walters-people/practice/nuxt-app/layouts/page-content.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="common-layout">
+		<el-container>
+			<PageHeader />
+			<PageTitle :title="getPageTitle" />
+			<el-main class="page-main">
+				<Nuxt />
+			</el-main>
+		</el-container>
+	</div>
+</template>
+
+<script>
+	import PageHeader from "@/components/Page/PageHeader";
+
+	export default {
+		"name": "PageContent",
+		"components": {
+			PageHeader,
+		},
+		"computed": {
+			getPageTitle() {
+				return this.formatPageTitle(this.$route.name);
+			},
+		},
+		"methods": {
+			formatPageTitle(text) {
+				const title = text.replace("-", " ");
+				const firstLetter = title.charAt(0);
+				const firstLetterCap = firstLetter.toUpperCase();
+				const remainingLetters = title.slice(1);
+				const capitalizedWord = firstLetterCap + remainingLetters;
+
+				return capitalizedWord;
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.page-main {
+		margin-bottom: 4rem;
+		padding: 0 2rem;
+	}
+</style>
--- companies/walters-people/practice/nuxt-app/pages/user-list.vue
+++ companies/walters-people/practice/nuxt-app/pages/user-list.vue
@@ -1,36 +1,20 @@
 <template>
 	<div class="page-main__inner">
-		<PageTitle :title="pageTitle" />
 		<UserListTable />
 		<UserMap />
 	</div>
 </template>
 
 <script>
-	import PageTitle from "@/components/Page/PageTitle";
 	import UserListTable from "@/components/User/UserListTable";
 	import UserMap from "@/components/User/UserMap";
 
 	export default {
 		"name": "UserList",
 		"components": {
-			PageTitle,
 			UserListTable,
 			UserMap,
 		},
-		data() {
-			return {
-				"pageTitle": `User list`,
-			};
-		},
+		"layout": "page-content",
 	};
 </script>
-
-<style lang="scss" scoped>
-	.page-main {
-		&__inner {
-			margin-bottom: 4rem;
-			padding: 0 2rem;
-		}
-	}
-</style>
