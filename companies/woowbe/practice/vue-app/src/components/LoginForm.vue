67da95bf10a717f0ebf5a785cd028d9d0e20a546
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-07-03 22:17:51 +0200

    feat(Login): create the `LoginForm` component and add it to the `Login` view
    Steps to follow:
    - Create the `LoginForm` component:
    The `LoginForm` component is now included in the `Login` view.
    It consists of email and password input fields.
    When the form is filled in and the button is clicked, the system validates the credentials.
    If the credentials are correct, the user is redirected to the `Account` page.
    However, if the credentials are incorrect, an error message is displayed to the user.
    - Update the `LoginView` view:
    Import and use the `LoginForm` component in the `Login` view.

---
+++ companies/woowbe/practice/vue-app/src/components/LoginForm.vue
@@ -0,0 +1,77 @@
+<template>
+	<form @submit.prevent="login">
+		<div>
+			<label for="email">Email:</label>
+			<input
+				type="email"
+				id="email"
+				v-model="email"
+				required
+			/>
+		</div>
+
+		<div>
+			<label for="password">Contraseña:</label>
+			<input
+				type="password"
+				id="password"
+				v-model="password"
+				required
+			/>
+		</div>
+
+		<button type="submit">Iniciar sesión</button>
+
+		<div>
+			<p v-if="errorMessage">
+				{{ errorMessage }}
+			</p>
+		</div>
+	</form>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			email: "",
+			password: "",
+			token: "",
+			errorMessage: "",
+		};
+	},
+	methods: {
+		async login() {
+			try {
+				const response = await fetch(
+					"https://backend.dev.woowbe.com/api/v1/auth/jwt/token/",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({
+							email: this.email,
+							password: this.password,
+						}),
+					},
+				);
+				if (response.ok) {
+					const data = await response.json();
+					this.token = data.token;
+					console.log("Inicio de sesión exitoso");
+					console.log("Token:", this.token);
+					this.$router.push("/account");
+				} else {
+					throw new Error(
+						"Error al iniciar sesión. Verifica tus credenciales.",
+					);
+				}
+			} catch (error) {
+				this.errorMessage = error.message;
+				console.error(error);
+			}
+		},
+	},
+};
+</script>
--- companies/woowbe/practice/vue-app/src/views/LoginView.vue
+++ companies/woowbe/practice/vue-app/src/views/LoginView.vue
@@ -1,16 +1,19 @@
 <template>
 	<div class="login">
 		<PageTitle :title="pageTitle" />
+		<LoginForm />
 	</div>
 </template>
 
 <script>
 import PageTitle from "@/components/PageTitle";
+import LoginForm from "@/components/LoginForm";
 
 export default {
 	name: "LoginView",
 	components: {
 		PageTitle,
+		LoginForm,
 	},
 	data() {
 		return {
