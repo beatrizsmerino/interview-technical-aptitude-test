58396c3719fa4fa0cac0c2905f46e860f0b99668
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-07-03 03:38:11 +0200

    refactor(PageHeader): extract the navigation to a new component
    Description
    - Any additions that you consider will improve usability, as well as UX/UI optimization with loadings, header/footer structuring, etc. will be valued.
    
    Steps:
    - Create the `PageHeader.vue` file as a new component.
    - Move the code above `#nav` from the `App.vue` file to `PageHeader.vue`.
    - Import and use the `PageHeader.vue` component in the `App.vue` file.
    - In the `PageHeader.vue` component, create a `linkList` data with an array of objects, where each one has the path and name of each link.
    - In the `PageHeader.vue` component, it creates a `getLinkList` computed property to get the list of links stored in the `linkList` data.
    - In the `PageHeader.vue` component, to keep the html clean and SEO compliant, a list has been created with `ul` and `li` tags. In it will be painted the list of links, obtained with `getListLink` computed, through a loop, with the `v-for` directive of vue.
    - In addition, the BEM and ITCSS architecture is used in the `#nav` styles. First changing the use of selectors with `id` to `classes`. Second avoiding nesting selectors using the `component+element+modifier` structure of BEM. And third, using the styles to create dividers between links, so that future links added follow the same structure without touching the HTML.

--- companies/woowbe/practice/vue-app/src/App.vue
+++ companies/woowbe/practice/vue-app/src/App.vue
@@ -1,15 +1,20 @@
 <template>
 	<div id="app">
-		<nav>
-			<router-link to="/">Home</router-link>
-			|
-			<router-link to="/about">About</router-link>
-			|
-			<router-link to="/login">Login</router-link>
-		</nav>
+		<PageHeader />
+
 		<router-view />
 	</div>
 </template>
+<script>
+import PageHeader from "@/components/PageHeader";
+
+export default {
+	name: "app",
+	components: {
+		PageHeader,
+	},
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -18,18 +23,5 @@
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	color: #2c3e50;
-}
-
-nav {
-	padding: 30px;
-
-	a {
-		font-weight: bold;
-		color: #2c3e50;
-
-		&.router-link-exact-active {
-			color: #42b983;
-		}
-	}
 }
 </style>
---
+++ companies/woowbe/practice/vue-app/src/components/PageHeader.vue
@@ -0,0 +1,87 @@
+<template>
+	<header class="page-header">
+		<nav class="nav">
+			<ul class="nav__list">
+				<li
+					class="nav__item"
+					v-for="link in getLinkList"
+					:key="link.id"
+				>
+					<router-link
+						class="nav__link"
+						:to="link.path"
+					>
+						{{ link.name }}
+					</router-link>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<script>
+export default {
+	name: "PageHeader",
+	data() {
+		return {
+			linkList: [
+				{
+					name: "Home",
+					path: "/",
+				},
+				{
+					name: "About",
+					path: "/about",
+				},
+				{
+					name: "Login",
+					path: "/login",
+				},
+			],
+		};
+	},
+	computed: {
+		getLinkList() {
+			return this.linkList;
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.nav {
+	padding: 30px;
+
+	&__list {
+		margin: 0;
+		padding: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		list-style: none;
+	}
+
+	&__item {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		&:not(:last-child) {
+			&:after {
+				content: "|";
+				display: inline-block;
+				margin: 0 0.3rem;
+			}
+		}
+	}
+
+	&__link {
+		font-weight: bold;
+		color: #2c3e50;
+
+		&.router-link-exact-active {
+			color: #42b983;
+		}
+	}
+}
+</style>
