6274003ee65af3ec4e2a3dfa3603e6c90e8fd64f
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-07-05 08:48:21 +0200

    refactor: move duplicated methods to `global-mixins`
    Reason:
    - In order to avoid code duplication, promote code reusability and enhances maintainability, this commit refactors the project by moving the duplicated methods to a global mixin called `global-mixins`.
    
    Steps:
    The following methods have been extracted to the `global-mixins`:
    - `isListArrayObject`: Checks if a value is an array containing objects.
    - `isListObject`: Checks if a value is an object (excluding arrays) with at least one key.
    - `isListArray`: Checks if a value is an array without objects.
    - `isImage`: Checks if a value is the path of an image file (supports .jpg, .jpeg, .png, .gif extensions).
    - `isIcon`: Checks if a value is the path of an icon file (supports .svg extension).
    - `isLink`: Checks if a value is a valid URL.
    
    Now these methods can be accessed globally and reused across multiple components by simply importing the `global-mixins` and adding it to the `mixins` option of a component.

--- companies/woowbe/practice/vue-app/src/components/Section/SectionBusiness.vue
+++ companies/woowbe/practice/vue-app/src/components/Section/SectionBusiness.vue
@@ -272,9 +272,11 @@
 
 <script>
 import { mapGetters } from "vuex";
+import globalMixins from "@/plugins/global-mixins.js";
 import UIMessage from "@/components/UI/UIMessage.vue";
 
 export default {
+	mixins: [globalMixins],
 	name: "SectionBusiness",
 	components: {
 		UIMessage,
@@ -317,66 +319,6 @@ export default {
 			const pageNumber = params.get("page");
 
 			return pageNumber ? pageNumber - 1 : "";
-		},
-		isEmpty(value) {
-			return (
-				value === null ||
-				(typeof value === "string" && value.trim() === "") ||
-				(Array.isArray(value) && value.length === 0) ||
-				(typeof value === "object" && Object.keys(value).length === 0)
-			);
-		},
-		isListArrayObject(value) {
-			if (Array.isArray(value)) {
-				return (
-					value.length > 0 &&
-					value.every(
-						item => typeof item === "object" && item !== null,
-					)
-				);
-			}
-			return false;
-		},
-		isListObject(value) {
-			return (
-				typeof value === "object" &&
-				value !== null &&
-				!Array.isArray(value) &&
-				Object.keys(value).length > 0
-			);
-		},
-		isListArray(value) {
-			return (
-				Array.isArray(value) &&
-				value.every(item => typeof item !== "object" || item === null)
-			);
-		},
-		isImage(value) {
-			if (typeof value === "string") {
-				const extensionsAvailable = [".jpg", ".jpeg", ".png", ".gif"];
-				const extension = value
-					.substring(value.lastIndexOf("."))
-					.toLowerCase();
-				return extensionsAvailable.includes(extension);
-			}
-			return false;
-		},
-		isIcon(value) {
-			if (typeof value === "string") {
-				const extensionsAvailable = [".svg"];
-				const extension = value
-					.substring(value.lastIndexOf("."))
-					.toLowerCase();
-				return extensionsAvailable.includes(extension);
-			}
-			return false;
-		},
-		isLink(value) {
-			if (typeof value === "string") {
-				const urlPattern = /^(ftp|http|https):\/\/[^ "]+$/;
-				return urlPattern.test(value);
-			}
-			return false;
 		},
 	},
 };
