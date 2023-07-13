6500485d8cc09f0bd2c237547ce88aa870aad625
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-07-13 08:42:12 +0200

    fix(UISelect): text too close to the arrow
    Error:
    - Forgot to take into account that the arrow takes up space, so the right padding should be larger.
    
    Solution:
    - Update the select spaces with the `padding` and `right` css properties.
    - Update the `font-size` of the select from the default styles.

--- companies/woowbe/practice/vue-app/src/components/UI/UISelect.vue
+++ companies/woowbe/practice/vue-app/src/components/UI/UISelect.vue
@@ -99,11 +99,12 @@
 			display: flex;
 			position: relative;
 			z-index: 1;
-			padding: 0.8rem;
+			padding: 0.5rem 2rem 0.5rem 0.8rem;
 			border: 0.15rem solid $color-silver;
 			border-radius: 0.5rem;
 			background-color: transparent;
 			color: $color-brand-2;
+			font-size: 1rem;
 			font-weight: 700;
 			cursor: pointer;
 			appearance: none;
@@ -121,7 +122,7 @@
 			position: absolute;
 			z-index: 1;
 			top: 50%;
-			right: 0.5rem;
+			right: 0.8rem;
 			width: 0;
 			height: 0;
 			transform: translateY(calc(-50% + 0.4rem / 2));
@@ -133,8 +134,12 @@
 
 		&--xs {
 			.select {
+				&__arrow {
+					right: 0.5rem;
+				}
+
 				&__field {
-					padding: 0.2rem 0.5rem;
+					padding: 0.2rem 1.8rem 0.2rem 0.5rem;
 					border-radius: 0.2rem;
 					font-size: 0.9rem;
 				}
